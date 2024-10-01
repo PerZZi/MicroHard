283935016dd705bf86f660516d4e7712430596b9
fedeval98 <fede.val9806@gmail.com>
2024-10-01 16:10:47 -0300

    primer subida

---
+++ src/main/java/opytha/microhard/MicrohardApplication.java
@@ -0,0 +1,13 @@
+package opytha.microhard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrohardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrohardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/opytha/microhard/MicrohardApplicationTests.java
@@ -0,0 +1,13 @@
+package opytha.microhard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrohardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
